931483de1e721261a34884966901d56852a77579
LHHDANG <142959277+LHHDANG@users.noreply.github.com>
2024-12-04 10:48:40 +0700

    thêm dbcontext

---
+++ backend/ARS-API/Services/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ARS_API.Models;
+namespace ARS_API.Services
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<StatusModel> statusModels { set; get;}
+        public DbSet<MediaModel> mediaModels { set; get;}
+    }
+}
