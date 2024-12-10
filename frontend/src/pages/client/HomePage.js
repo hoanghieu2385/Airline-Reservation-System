08624af49813f8370c37077f91082995c98729b6
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-12-10 11:21:12 +0700

    fix tam search form cho index

--- frontend/src/pages/client/HomePage.js
+++ frontend/src/pages/client/HomePage.js
@@ -1,6 +1,7 @@
 // File: src/pages/HomePage.js
 import React from 'react';
 import '../../assets/css/HomePage.css';
+import SearchForm from '../../components/client/SearchForm';
 
 const HomePage = () => {
     return (
@@ -12,27 +13,8 @@ const HomePage = () => {
                         Temporas Facere Doloribus Id Aut. Ea Maiores Esse Accusantium Laboriosam.
                         Quos Commodi Non Assumenda Quam Illum.
                     </p>
-                    <form className="search-form">
-                        <div className="row">
-                            <div className="col-md-3">
-                                <input type="text" className="form-control" placeholder="From" />
-                            </div>
-                            <div className="col-md-3">
-                                <input type="text" className="form-control" placeholder="To" />
-                            </div>
-                            <div className="col-md-2">
-                                <input type="date" className="form-control" placeholder="Departure" />
-                            </div>
-                            <div className="col-md-2">
-                                <input type="date" className="form-control" placeholder="Return" />
-                            </div>
-                            <div className="col-md-2">
-                                <button type="submit" className="btn btn-primary">
-                                    Search Flights
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    
+                    <SearchForm />
                 </div>
             </section>
         </div>
