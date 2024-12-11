dbb002e5b4c924cac86df5f1fee4a95ac859f396
hoanghieu2385 <hoangminhhieu230805@gmail.com>
2024-12-11 08:19:24 +0700

    làm thêm why choose us ở home client

--- frontend/src/pages/client/HomePage.js
+++ frontend/src/pages/client/HomePage.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import '../../assets/css/HomePage.css';
 import SearchForm from '../../components/client/SearchForm';
+import img_why_choose_us from '../../assets/images/flights_homepage.png';
 
 const HomePage = () => {
     return (
@@ -15,6 +16,31 @@ const HomePage = () => {
                     </p>
                     
                     <SearchForm />
+                </div>
+            </section>
+
+            <section className="why-choose-us">
+                <div className="container">
+                    <div className="why-choose-us-content">
+                        {/* Image */}
+                        <div className="why-choose-us-image">
+                            <img src={img_why_choose_us} alt="Beautiful Destination" className='img_why_choose_us' />
+                        </div>
+
+                        {/* Text Content */}
+                        <div className="why-choose-us-text">
+                            <h2 className="section-title">Why Choose Us</h2>
+                            <p className="section-subtitle">
+                                Temporas Facere Doloribus Id Aut. Ea Maiores Esse Accusantium Laboriosam. Quos Commodi Non Assumenda Quam Illum.
+                            </p>
+                            <ul className="features-list">
+                                <li>Assumenda Nobis Sit Deserunt Dolorem Repudiandae Occaecati Quisquam.</li>
+                                <li>Beatae Aut Beatae Sed Aliquid Et Accusamus Vel.</li>
+                                <li>Dolores Qui Nihil Quaerat Ducimus Fugit Aut Praesentium.</li>
+                                <li>Necessitatibus Ut Culpa Molestias Deleniti Porro Maxime Enim Sed Vel.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
