559bdba861bb299ff9fdb068c63407ba2f8979f6
LHHDANG <142959277+LHHDANG@users.noreply.github.com>
2024-12-26 16:39:25 +0700

    fix lỗi register và thêm authorize

--- backend/ARS-API/Controllers/EticketController.cs
+++ backend/ARS-API/Controllers/EticketController.cs
@@ -1,5 +1,6 @@
 using ARS_API.DTOs;
 using ARS_API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ namespace ARS_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class EticketController : ControllerBase
     {
         private readonly ApplicationDBContext _dbContext;
